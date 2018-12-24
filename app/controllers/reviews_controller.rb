ee8b81032bd07e4e1f4c6b3793d2149b53e93a8f
kduane <karl.duane@gmail.com>
2018-12-24 11:41:17 -0500

    core stories complete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,10 +11,10 @@ class ReviewsController < ApplicationController
 
   def show
     @review = Review.find(params[:id])
-    binding.pry
+    @city = City.find(@review.city_id)
+    # binding.pry
     render :show
   end
-
 
   def create
     @city = City.find(params[:city_id])
@@ -29,6 +29,31 @@ class ReviewsController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @review = Review.find(params[:id])
+    @city = City.find(@review.city_id)
+    render :edit
+
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @city = City.find(@review.city_id)
+    if @review.update_attributes(review_params)
+      flash[:notice] = 'Thanks for updating!'
+      redirect_to @review
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to @question
+  end
+
 
   private
 
